f7c7bf907d3b42b55492f0751260556e7e9b17ad
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2023-04-01 06:52:17 +0200

    Add a button to the top of the screen.
    Add a button to the top of the screen that will send the user
     a popup asking for the number of squares per side for the new grid.
      Once entered, the existing grid would be removed and a new grid
      will be generated.
    + Set the limit for the user input to a maximum of 50.

--- function .js
+++ function .js
@@ -8,21 +8,44 @@ container.classList.add("container");
 // Adding container
 body.appendChild(container);
 
-//Adding grid of 16*16 grid of divs
-function addNumOfRowsOfNumOfDivs() {
-  for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let i = 0; i < 16; i++) {
-      const div = document.createElement("div");
-      div.classList.add("square");
-      row.appendChild(div);
+// Add buttun for the user to add the number of squares he'dlike
+
+const button = document.createElement("button");
+button.textContent = "Sketch on";
+container.appendChild(button);
+
+//fucntion of creating and resiting sketch on comand by clicking sketch on button
+function addNumOfRowsOfNumOfDivs(e) {
+  // Resetinf for new sketchpad
+
+  if (container.contains(document.querySelector(".row"))) {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((element) => {
+      container.removeChild(element);
+    });
+  }
+  // Getting the number of squares per side the user wants
+  const numOfSquares = prompt(
+    `Please Enter the number of squares per side you'd like the sketch to have. 
+    Max 50`
+  );
+  // number of squers per side limit check
+  if (numOfSquares <= 50) {
+    //Adding grid of row of divs <= the 100 as the user wants
+    for (let i = 0; i < numOfSquares; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      container.appendChild(row);
+      for (let i = 0; i < numOfSquares; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        row.appendChild(div);
+      }
     }
+  } else {
+    addNumOfRowsOfNumOfDivs();
   }
 }
-
-addNumOfRowsOfNumOfDivs();
 
 //Eventlistner for the hover effect of painting
 window.addEventListener("mouseover", addBackgroundColor);
@@ -33,3 +56,6 @@ function addBackgroundColor(e) {
   }
   e.target.classList.add("colored");
 }
+
+//Eventlistner for the sketch on button
+button.addEventListener("click", addNumOfRowsOfNumOfDivs);
