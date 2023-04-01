dbb7e90b1f7605641d6c8523b9e52402acc45da5
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2023-04-01 06:25:56 +0200

    Adding Container element to hold the grid inside.

---
+++ function .js
@@ -0,0 +1,9 @@
+//Add varribles
+const body = document.querySelector("body");
+const container = document.createElement("div");
+
+//Add classes
+container.classList.add("container");
+
+// Adding container
+body.appendChild(container);
