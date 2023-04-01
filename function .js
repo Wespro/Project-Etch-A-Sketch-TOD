6e27903e8a74f9073a10a379bae0009786458aff
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2023-04-01 06:33:14 +0200

    Add grid of 16*16 grid of divs

--- function .js
+++ function .js
@@ -7,3 +7,19 @@ container.classList.add("container");
 
 // Adding container
 body.appendChild(container);
+
+//Adding grid of 16*16 grid of divs
+function addNumOfRowsOfNumOfDivs() {
+  for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let i = 0; i < 16; i++) {
+      const div = document.createElement("div");
+      div.classList.add("square");
+      row.appendChild(div);
+    }
+  }
+}
+
+addNumOfRowsOfNumOfDivs();
